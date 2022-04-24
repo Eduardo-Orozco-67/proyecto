e0ab4bdd8275ae4cc31f9ff9e20de0e335539173
eduardo orozco <eduardo.orozco66@unach.mx>
2022-04-24 00:49:40 -0500

    Merge

--- hospital.sql
+++ hospital.sql
@@ -31,7 +31,11 @@ create table Cita
 	id_cita integer not NULL GENERATED ALWAYS AS IDENTITY,
 	num_paciente integer not null,
 	consultorio varchar not null,
+<<<<<<< HEAD
 	fecha_hora_cita TIMESTAMP not null constraint fecha_invalida check (fecha_hora_cita < now()),
+=======
+	fecha_hora_con TIMESTAMP not null constraint fecha_invalida check (fecha > now()),
+>>>>>>> e3afe2b (corrigiendo fecha y hora)
 	constraint Cita_pkey primary key(id_cita),
 	constraint Cita_fkey foreign key(num_paciente) references Paciente(num_paciente)
 );
